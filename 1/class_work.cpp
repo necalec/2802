e4c22761a3cae01d196fd9e7c2c3b33aa9a637f1
Nikita Zhukov <necalec@gmail.com>
2024-04-16 00:35:10 +0300

    first

---
+++ 1/class_work.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <omp.h>
+
+int main()
+{
+    int size_threads;
+    std::cin >> size_threads;
+
+#pragma omp parallel shared(size_threads)
+    {
+        int rank;
+
+#pragma omp for nowait
+        for (int i = 0; i < size_threads; i++) {
+            rank = omp_get_thread_num();
+            std::cout << "I am thread " << rank << std::endl;
+        }
+    }
+
+    return 0;
+}
---
+++ 2/class_job.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <omp.h>
+
+int main()
+{
+    omp_lock_t writelock;
+    omp_init_lock(&writelock);
+
+    int size_threads;
+    std::cin >> size_threads;
+    int rank;
+
+#pragma omp parallel shared(size_threads) private(rank)
+    {
+#pragma omp for nowait
+        for (int i = 0; i < size_threads; i++) {
+            omp_set_lock(&writelock);
+            rank = omp_get_thread_num();
+            std::cout << "I am thread " << rank << std::endl;
+            omp_unset_lock(&writelock);
+        }
+    }
+
+    return 0;
+}
